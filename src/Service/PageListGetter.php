e27fc0b5ee31cb078fdaf5566c6de9ba32b898ec
addshore <addshorewiki@gmail.com>
2014-07-06 14:43:02 +0100

    Add getPageListFromTransclusions method

--- src/Service/PageListGetter.php
+++ src/Service/PageListGetter.php
@@ -70,4 +70,42 @@ class PageListGetter {
 		}
 	}
 
+	/**
+	 * @param string $pageName
+	 * @param array $namespaces
+	 *
+	 * @return Pages
+	 */
+	public function getPageListFromPageTransclusions( $pageName, $namespaces= array() ) {
+		$continue = '';
+		$pages = new Pages();
+
+		while ( true ) {
+			$params = array(
+				'list' => 'embeddedin',
+				'eititle' => $pageName,
+				'eilimit' => 500,
+				'einamespace' => implode( '|', $namespaces )
+			);
+			if( !empty( $continue ) ) {
+				$params['eicontinue'] = $continue;
+			}
+			$result = $this->api->getAction( 'query', $params );
+
+			foreach ( $result['query']['embeddedin'] as $member ) {
+				$pages->addPage( new Page(
+						$member['title'],
+						$member['pageid'],
+						new Revisions()
+					)
+				);
+			}
+			if ( empty( $result['query-continue']['embeddedin']['eicontinue'] ) ) {
+				return $pages;
+			} else {
+				$continue = $result['query-continue']['embeddedin']['eicontinue'];
+			}
+		}
+	}
+
 }
