90b016789a2aee5844829c0eb8977c5194fc52e6
addshore <addshorewiki@gmail.com>
2014-07-08 17:13:54 +0100

    Implement UserRightsChanger
    Fixes #6

--- src/Service/UserRightsChanger.php
+++ src/Service/UserRightsChanger.php
@@ -2,6 +2,7 @@
 
 namespace Mediawiki\Api\Service;
 
+use Mediawiki\Api\MediawikiApi;
 use Mediawiki\DataModel\User;
 
 /**
@@ -10,14 +11,50 @@ use Mediawiki\DataModel\User;
 class UserRightsChanger {
 
 	/**
+	 * @var MediawikiApi
+	 */
+	private $api;
+
+	/**
+	 * @param MediawikiApi $api
+	 */
+	public function __construct( MediawikiApi $api ) {
+		$this->api = $api;
+	}
+
+	/**
 	 * @since 0.3
 	 *
 	 * @param User $user
-	 * @param $rights
+	 * @param string[] $add
+	 * @param string[] $remove
+	 * @param string $reason
+	 *
+	 * @return bool
 	 */
-	public function change( User $user, $rights ) {
-		//TODO implement me
-		throw new \BadMethodCallException( 'Not yet implemented' );
+	public function change( User $user, $add = array(), $remove = array(), $reason = '' ) {
+		$result = $this->api->postAction( 'query', array(
+			'list' => 'users',
+			'ustoken' => 'userrights',
+			'ususers' => $user->getName(),
+		) );
+
+		$params = array(
+			'user' => $user->getName(),
+			'token' => $result['query']['users'][0]['userrightstoken'],
+		);
+		if( !empty( $reason ) ) {
+			$params['reason'] = $reason;
+		}
+		if( !empty( $add ) ) {
+			$params['add'] = implode( '|', $add );
+		}
+		if( !empty( $remove ) ) {
+			$params['remove'] = implode( '|', $remove );
+		}
+
+		$this->api->postAction( 'userrights', $params );
+		return true;
 	}
 
 } 
