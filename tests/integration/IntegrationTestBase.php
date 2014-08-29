595e70026e76297cdfc90c3d1fc2386ccdd123e4
addshore <addshorewiki@gmail.com>
2014-08-29 22:39:44 +0100

    Split tests ready for unit and integration tests

---
+++ tests/integration/IntegrationTestBase.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Mediawiki\Api\Test;
+
+use Mediawiki\Api\MediawikiApi;
+use Mediawiki\Api\MediawikiFactory;
+
+class IntegrationTestBase extends \PHPUnit_Framework_TestCase {
+
+	/**
+	 * @var MediawikiFactory
+	 */
+	protected $factory;
+
+	protected function setUp() {
+		parent::setUp();
+		$this->factory = new MediawikiFactory(
+			new MediawikiApi( 'http://localhost/w/api.php' )
+		);
+	}
+
+	// Needed to stop phpunit complaining
+	public function testFactory() {
+		$this->assertInstanceOf( 'Wikibase\Api\MediawikiFactory', $this->factory );
+	}
+
+	protected function tearDown() {
+		parent::tearDown();
+		unset( $this->factory );
+	}
+
+}
--- tests/integration/bootstrap.php
+++ tests/integration/bootstrap.php
@@ -4,15 +4,15 @@ if ( php_sapi_name() !== 'cli' ) {
 	die( 'Not an entry point' );
 }
 
-if ( !is_readable( __DIR__ . '/../vendor/autoload.php' ) ) {
+if ( !is_readable( __DIR__ . '/../../vendor/autoload.php' ) ) {
 	die( 'You need to install this package with Composer before you can run the tests' );
 }
 
 $pwd = getcwd();
-chdir( __DIR__ . '/../' );
+chdir( __DIR__ . '/../../' );
 passthru( 'composer dump-autoload' );
 chdir( $pwd );
 
-$autoloader = require_once( __DIR__ . '/../vendor/autoload.php' );
+$autoloader = require_once( __DIR__ . '/../../vendor/autoload.php' );
 
 $autoloader->addPsr4( 'Mediawiki\\Api\\Test\\', __DIR__ );
