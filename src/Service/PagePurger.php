46a834a72e4d49bd5d77059966ff1956a3047c8b
blackspirit96 <panagiotis_11@hotmail.com>
2016-11-03 22:46:44 +0200

    implement purgePages() return Pages object, purge() return boolean
    * purge()
    implemented the logic to return true or false
    when the page was successfully purged
    
    * purgePages()
    return Pages object containing only the successfully
    purged pages

--- src/Service/PagePurger.php
+++ src/Service/PagePurger.php
@@ -40,14 +40,17 @@ class PagePurger {
 	 *
 	 * @param Page $page the page that is going to be purged
 	 *
-	 * @return bool
+	 * @return bool return true if the purge was successful
 	 */
 	public function purge( Page $page ) {
-		$this->api->postRequest(
+		$responseArray = $this->api->postRequest(
 			new SimpleRequest( 'purge', [ 'pageids' => $page->getId() ] )
 		);
 
-		return true;
+		// the purge response for the page
+		$purgeResponse = $responseArray['purge'][0];
+
+		return array_key_exists( 'purged' , $purgeResponse );
 	}
 
 	/**
@@ -62,7 +65,7 @@ class PagePurger {
 	 *
 	 * @param Pages $pages the pages that are going to be purged
 	 *
-	 * @return bool
+	 * @return Pages the pages that have been purged successfully
 	 */
 	public function purgePages( Pages $pages ) {
 		$pagesArray = $pages->toArray();
@@ -76,13 +79,40 @@ class PagePurger {
 		// because the mediawiki api require multiple
 		// values to be seperated like the example
 		// ex: [111, 222, 333] => "111|222|333"
-		$pageIdsMultiple = implode( "|", $pagesIds );
+		$pageIdsMultiple = implode( '|', $pagesIds );
 
-		$this->api->postRequest(
+		$responseArray = $this->api->postRequest(
 			new SimpleRequest( 'purge', [ 'pageids' => $pageIdsMultiple ] )
 		);
 
-		return true;
+		// array that will hold the successfully purged pages
+		$purgedPages = new Pages();
+
+		// for every purge result
+		foreach ( $responseArray['purge'] as $purgeResponse ) {
+
+			// if the purge for the page was successful
+			if ( array_key_exists( 'purged' , $purgeResponse ) ) {
+
+				// we iterate all the input pages
+				foreach ( $pagesArray as $page ) {
+
+					// and if the page from the input was successfully purged
+					if ( $purgeResponse['title'] === $page->getTitle()->getText() ) {
+
+						// add it in the purgedPages object
+						$purgedPages->addPage( $page );
+
+						break;
+					}
+
+				}
+
+			}
+
+		}
+
+		return $purgedPages;
 	}
 
 	/**
