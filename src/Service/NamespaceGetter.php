bbde36cd66cebb8e3fa55fa9087dc4c2cc765bc3
Gabriel Birke <gabriel.birke@gmail.com>
2017-01-06 23:30:00 +0100

    Add NamespaceGetter service
    Add service class and unit test. The test mocks the API by using a JSON
    fixture as API result instead of using a MediaWiki instance.

---
+++ src/Service/NamespaceGetter.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Mediawiki\Api\Service;
+
+use Mediawiki\Api\MediawikiApi;
+use Mediawiki\Api\SimpleRequest;
+use Mediawiki\DataModel\NamespaceInfo;
+
+/**
+ * @author gbirke
+ */
+class NamespaceGetter
+{
+	private $api;
+
+	public function __construct( MediawikiApi $api ) {
+
+		$this->api = $api;
+	}
+
+	/**
+	 * @param string $canonicalName
+	 * @return NamespaceInfo|null
+	 */
+	public function getNamespaceByCanonicalName( $canonicalName ) {
+		foreach ( $this->getNamespaceResult()['query']['namespaces'] as $nsInfo ) {
+			if ( !empty( $nsInfo['canonical'] ) && $nsInfo['canonical'] === $canonicalName ) {
+				return $this->createNamespaceFromQuery( $nsInfo );
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * @param string $name
+	 * @return NamespaceInfo|null
+	 */
+	public function getNamespaceByName( $name ) {
+		foreach ( $this->getNamespaceResult()['query']['namespaces'] as $nsInfo ) {
+			if ( ( !empty( $nsInfo['canonical'] ) && $nsInfo['canonical'] === $name ) ||
+				$nsInfo['*'] === $name ) {
+				return $this->createNamespaceFromQuery( $nsInfo );
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * @return NamespaceInfo[]
+	 */
+	public function getNamespaces() {
+		$namespaces = [];
+		foreach ( $this->getNamespaceResult()['query']['namespaces'] as $nsInfo ) {
+			$namespaces[$nsInfo['id']] = $this->createNamespaceFromQuery( $nsInfo );
+		}
+		return $namespaces;
+	}
+
+	private function createNamespaceFromQuery( $nsInfo ) {
+		return new NamespaceInfo(
+			$nsInfo['id'],
+			empty( $nsInfo['canonical'] ) ? '' : $nsInfo['canonical'],
+			$nsInfo['*'],
+			$nsInfo['case'],
+			empty( $nsInfo['defaultcontentmodel'] ) ? null : $nsInfo['defaultcontentmodel']
+		);
+	}
+
+	/**
+	 * @return array
+	 */
+	private function getNamespaceResult() {
+		return $this->api->getRequest( new SimpleRequest(
+			'query', [
+				'meta' => 'siteinfo',
+				'siprop' => 'namespaces|namespacealiases'
+			]
+		) );
+	}
+
+}
