0c8a7e5ef4358924a8c07ce18ee2731c9c001958
addwiki-ci <ci@addwiki.github.io>
2022-01-08 10:46:47 +0000

    psalm fixes (#140)

--- src/CategoryLoopException.php
+++ src/CategoryLoopException.php
@@ -20,9 +20,9 @@ class CategoryLoopException extends Exception {
 	/**
 	 * Get the path of Pages that comprise the category loop. The first item in this list is also a
 	 * child page of the last item.
-	 * @return Pages The set of category Pages that comprise the category loop.
+	 * @return Pages|null The set of category Pages that comprise the category loop.
 	 */
-	public function getCategoryPath(): Pages {
+	public function getCategoryPath(): ?Pages {
 		return $this->categoryPath;
 	}
 
